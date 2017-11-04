b6eda924f1372977e5788a05194655393fd5edf8
Brennan Gamwell <brennan@segment.com>
2017-11-04 09:49:45 -0700

    Update/dependencies (#13)
    * Update gradle deps to resolve IDE errors
    * Apply javafmt plugin

--- src/main/java/com/segment/analytics/android/integrations/countly/CountlyIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/countly/CountlyIntegration.java
@@ -24,26 +24,34 @@ import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
  * @see <a href="https://github.com/Countly/countly-sdk-android">Countly Android SDK</a>
  */
 public class CountlyIntegration extends Integration<Countly> {
-  public static final Factory FACTORY = new Factory() {
-    @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
-      String serverUrl = settings.getString("serverUrl");
-      String appKey = settings.getString("appKey");
-      return new CountlyIntegration(Countly.sharedInstance(), analytics.logger(COUNTLY_KEY),
-          analytics.getApplication(), serverUrl, appKey);
-    }
+  public static final Factory FACTORY =
+      new Factory() {
+        @Override
+        public Integration<?> create(ValueMap settings, Analytics analytics) {
+          String serverUrl = settings.getString("serverUrl");
+          String appKey = settings.getString("appKey");
+          return new CountlyIntegration(
+              Countly.sharedInstance(),
+              analytics.logger(COUNTLY_KEY),
+              analytics.getApplication(),
+              serverUrl,
+              appKey);
+        }
 
-    @Override public String key() {
-      return COUNTLY_KEY;
-    }
-  };
+        @Override
+        public String key() {
+          return COUNTLY_KEY;
+        }
+      };
   private static final String VIEWED_EVENT_FORMAT = "Viewed %s Screen";
   private static final String COUNTLY_KEY = "Countly";
 
   final Countly countly;
   final Logger logger;
 
-  CountlyIntegration(Countly countly, Logger logger, Context context, String serverUrl,
-      String appKey) throws IllegalStateException {
+  CountlyIntegration(
+      Countly countly, Logger logger, Context context, String serverUrl, String appKey)
+      throws IllegalStateException {
     this.countly = countly;
     this.logger = logger;
     boolean loggingEnabled = logger.logLevel == INFO || logger.logLevel == VERBOSE;
@@ -53,28 +61,33 @@ public class CountlyIntegration extends Integration<Countly> {
     logger.verbose("countly.init(context, %s, %s)", serverUrl, appKey);
   }
 
-  @Override public Countly getUnderlyingInstance() {
+  @Override
+  public Countly getUnderlyingInstance() {
     return countly;
   }
 
-  @Override public void onActivityStarted(Activity activity) {
+  @Override
+  public void onActivityStarted(Activity activity) {
     super.onActivityStarted(activity);
     countly.onStart(activity);
     logger.verbose("countly.onStart(activity)");
   }
 
-  @Override public void onActivityStopped(Activity activity) {
+  @Override
+  public void onActivityStopped(Activity activity) {
     super.onActivityStopped(activity);
     countly.onStop();
     logger.verbose("countly.onStop()");
   }
 
-  @Override public void track(TrackPayload track) {
+  @Override
+  public void track(TrackPayload track) {
     super.track(track);
     event(track.event(), track.properties());
   }
 
-  @Override public void screen(ScreenPayload screen) {
+  @Override
+  public void screen(ScreenPayload screen) {
     super.screen(screen);
     event(String.format(VIEWED_EVENT_FORMAT, screen.event()), screen.properties());
   }
